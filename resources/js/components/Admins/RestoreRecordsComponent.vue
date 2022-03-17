c5fd7e3eb7cbe5d00b3b507d2dae7139969ee042
juan rosas <juan.rosasr01@gmail.com>
2022-03-17 11:55:32 -0400

    agregado boton para ir atras en vista de restaurar registros

--- resources/js/components/Admins/RestoreRecordsComponent.vue
+++ resources/js/components/Admins/RestoreRecordsComponent.vue
@@ -5,14 +5,15 @@
                 <div class="card-header">
                     <h6 class="card-title">
                         Restaurar Registros eliminados
-                        <a href="javascript:void(0)" title="haz click para ver la ayuda guiada de este elemento"
-                           data-toggle="tooltip" class="btn-help" @click="initUIGuide(helpFile)">
+                        <a href="javascript:void(0)" title="haz click para ver la ayuda guiada de este elemento" data-toggle="tooltip" class="btn-help" @click="initUIGuide(helpFile)">
                             <i class="ion ion-ios-help-outline cursor-pointer"></i>
                         </a>
                     </h6>
                     <div class="card-btns">
-                        <a href="javascript:void(0)" class="card-minimize btn btn-card-action btn-round"
-                           title="Minimizar" data-toggle="tooltip">
+                        <a :href="route_previous" class="btn btn-sm btn-primary btn-custom" title="Ir atrás" data-toggle="tooltip" v-has-tooltip>
+                            <i class="fa fa-reply"></i>
+                        </a>
+                        <a href="javascript:void(0)" class="card-minimize btn btn-card-action btn-round" title="Minimizar" data-toggle="tooltip">
                             <i class="now-ui-icons arrows-1_minimal-up"></i>
                         </a>
                     </div>
@@ -27,8 +28,7 @@
                                 <span class="input-group-addon">
                                     <i class="now-ui-icons ui-1_calendar-60"></i>
                                 </span>
-                                <input type="date" class="form-control" data-toggle="tooltip" title="Desde la fecha"
-                                       v-model="start_delete_at" id="startDeleteAt" placeholder="Fecha">
+                                <input type="date" class="form-control" data-toggle="tooltip" title="Desde la fecha" v-model="start_delete_at" id="startDeleteAt" placeholder="Fecha">
                             </div>
                         </div>
                         <div class="form-group col-md-2" id="helpRestoreFilterToDate">
@@ -36,8 +36,7 @@
                                 <span class="input-group-addon">
                                     <i class="now-ui-icons ui-1_calendar-60"></i>
                                 </span>
-                                <input type="date" class="form-control" data-toggle="tooltip" title="Hasta la fecha"
-                                       v-model="end_delete_at" id="endDeleteAt" placeholder="Fecha">
+                                <input type="date" class="form-control" data-toggle="tooltip" title="Hasta la fecha" v-model="end_delete_at" id="endDeleteAt" placeholder="Fecha">
                             </div>
                         </div>
                         <div class="form-group col-md-2" id="helpRestoreFilterModule">
@@ -49,8 +48,7 @@
                             </select>
                         </div>
                         <div class="form-group col-md-2" id="helpRestoreFilterButton">
-                            <button type="button" class="btn btn-info btn-icon btn-xs px-3" data-toggle="tooltip"
-                                    title="Buscar registros eliminados" @click="readRecords">
+                            <button type="button" class="btn btn-info btn-icon btn-xs px-3" data-toggle="tooltip" title="Buscar registros eliminados" @click="readRecords">
                                 <i class="fa fa-search"></i>
                             </button>
                         </div>
@@ -69,9 +67,7 @@
                             <v-client-table :columns="columns" :data="records" :options="table_options">
                                 <div slot="registers" slot-scope="props" v-html="props.row.registers"></div>
                                 <div slot="id" slot-scope="props" class="text-center">
-                                    <button @click="restore(props.row.module, props.row.id)"
-                                            class="btn btn-success btn-xs btn-icon btn-action"
-                                            title="Restaurar registro" data-toggle="tooltip" type="button">
+                                    <button @click="restore(props.row.module, props.row.id)" class="btn btn-success btn-xs btn-icon btn-action" title="Restaurar registro" data-toggle="tooltip" type="button">
                                         <i class="fa fa-check"></i>
                                     </button>
                                 </div>
@@ -83,121 +79,125 @@
         </div>
     </div>
 </template>
-
 <script>
-    export default {
-        data() {
-            return {
-                start_delete_at: '',
-                end_delete_at: '',
-                module_delete_at: '',
-                records: [],
-                columns: ['deleted_at', 'module', 'registers', 'id'],
-            }
+export default {
+    data() {
+        return {
+            start_delete_at: '',
+            end_delete_at: '',
+            module_delete_at: '',
+            records: [],
+            columns: ['deleted_at', 'module', 'registers', 'id'],
+        }
+    },
+    props: {
+        modules: {
+            type: Array,
+            required: false,
+            default: null
         },
-        props: {
-            modules: {
-                type: Array,
-                required: false,
-                default: null
-            }
+        route_previous: {
+            type: String,
+            required: true,
+            default: '/',
         },
-        watch: {
-            start_delete_at: function() {
-                const vm = this;
-                $('#endDeteleAt').attr('min', vm.start_delete_at);
-            },
-            end_delete_at: function() {
-                const vm = this;
-                if (vm.end_delete_at) {
-                    $('#startDeleteAt').attr('max', vm.end_delete_at);
-                } else {
-                    if (!$('#startDeleteAt').hasClass('no-restrict')) {
-                        $('#startDeleteAt').attr('max', vm.getCurrentDate());
-                    }
+    },
+    watch: {
+        start_delete_at: function() {
+            const vm = this;
+            $('#endDeteleAt').attr('min', vm.start_delete_at);
+        },
+        end_delete_at: function() {
+            const vm = this;
+            if (vm.end_delete_at) {
+                $('#startDeleteAt').attr('max', vm.end_delete_at);
+            } else {
+                if (!$('#startDeleteAt').hasClass('no-restrict')) {
+                    $('#startDeleteAt').attr('max', vm.getCurrentDate());
                 }
             }
-        },
-        methods: {
-            /**
-             * Restaurar registro eliminado
-             *
-             * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
-             */
-            restore(moduleClass, id) {
-                const vm = this;
-                bootbox.confirm('Está seguro de restaurar el registro seleccionado?', function (result) {
-                    if (result) {
-                        vm.loading = true;
-                        axios.post('/app/restore-record', {
-                            module: moduleClass,
-                            id: id
-                        }).then(response => {
-                            if (response.data.result) {
-                                vm.readRecords();
-                                vm.showMessage(
-                                    'custom', 'Éxito', 'success', 'screen-ok', 'Registro restaurado con éxito'
-                                );
-                            }
-                            vm.loading = false;
-                        }).catch(error => {
-                            console.error(error);
-                        });
-                    }
-                });
-            },
-            /**
-             * Método que obtiene los registros a mostrar
-             *
-             * @author  Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
-             *
-             * @param  {string} url Ruta que obtiene todos los registros solicitados
-             */
-            async readRecords() {
-                const vm = this;
-                vm.loading = true;
-                await axios.post('/app/deleted-records', {
-                    start_delete_at: vm.start_delete_at,
-                    end_delete_at: vm.end_delete_at,
-                    module_delete_at: vm.module_delete_at
-                }).then(response => {
-                    if (response.data.result && typeof(response.data.records) !== "undefined") {
-                        vm.records = response.data.records;
-                        vm.records.sort(
-                            (a,b) => (a.deleted_at > b.deleted_at) ? 1 : ((b.deleted_at > a.deleted_at) ? -1 : 0)
-                        );
-                    }
-                }).catch(error => {
-                    console.error(error);
-                });
-                vm.loading = false;
-            },
-        },
-        created() {
-            const vm = this;
-            vm.table_options.headings = {
-                'deleted_at': 'Fecha',
-                'module': 'Módulo',
-                'registers': 'Registros',
-                'id': 'Acción'
-            };
-            vm.table_options.sortable = ['deleted_at', 'module', 'registers'];
-            vm.table_options.filterable = ['deleted_at', 'module', 'registers'];
-            vm.table_options.columnsClasses = {
-                'deleted_at': 'col-md-1',
-                'module': 'col-md-3',
-                'registers': 'col-md-7',
-                'id': 'col-md-1'
-            };
-        },
-        mounted() {
-            const vm = this;
-
-            vm.readRecords();
-
-            $('#restoreSearchModule').on('change', function() {
-                vm.module_delete_at = $(this).val();
-            });
         }
-    };
+    },
+    methods: {
+        /**
+         * Restaurar registro eliminado
+         *
+         * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+         */
+        restore(moduleClass, id) {
+            const vm = this;
+            bootbox.confirm('Está seguro de restaurar el registro seleccionado?', function(result) {
+                if (result) {
+                    vm.loading = true;
+                    axios.post('/app/restore-record', {
+                        module: moduleClass,
+                        id: id
+                    }).then(response => {
+                        if (response.data.result) {
+                            vm.readRecords();
+                            vm.showMessage(
+                                'custom', 'Éxito', 'success', 'screen-ok', 'Registro restaurado con éxito'
+                            );
+                        }
+                        vm.loading = false;
+                    }).catch(error => {
+                        console.error(error);
+                    });
+                }
+            });
+        },
+        /**
+         * Método que obtiene los registros a mostrar
+         *
+         * @author  Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+         *
+         * @param  {string} url Ruta que obtiene todos los registros solicitados
+         */
+        async readRecords() {
+            const vm = this;
+            vm.loading = true;
+            await axios.post('/app/deleted-records', {
+                start_delete_at: vm.start_delete_at,
+                end_delete_at: vm.end_delete_at,
+                module_delete_at: vm.module_delete_at
+            }).then(response => {
+                if (response.data.result && typeof(response.data.records) !== "undefined") {
+                    vm.records = response.data.records;
+                    vm.records.sort(
+                        (a, b) => (a.deleted_at > b.deleted_at) ? 1 : ((b.deleted_at > a.deleted_at) ? -1 : 0)
+                    );
+                }
+            }).catch(error => {
+                console.error(error);
+            });
+            vm.loading = false;
+        },
+    },
+    created() {
+        const vm = this;
+        vm.table_options.headings = {
+            'deleted_at': 'Fecha',
+            'module': 'Módulo',
+            'registers': 'Registros',
+            'id': 'Acción'
+        };
+        vm.table_options.sortable = ['deleted_at', 'module', 'registers'];
+        vm.table_options.filterable = ['deleted_at', 'module', 'registers'];
+        vm.table_options.columnsClasses = {
+            'deleted_at': 'col-md-1',
+            'module': 'col-md-3',
+            'registers': 'col-md-7',
+            'id': 'col-md-1'
+        };
+    },
+    mounted() {
+        const vm = this;
+
+        vm.readRecords();
+
+        $('#restoreSearchModule').on('change', function() {
+            vm.module_delete_at = $(this).val();
+        });
+    }
+};
 </script>
