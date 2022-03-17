820cf6f973d9660e62a45967a3f1d14c46e97481
juan rosas <juan.rosasr01@gmail.com>
2022-03-17 10:56:42 -0400

    Finance: agregado App\Traits\ModelsTrait a modelos

--- modules/Finance/Models/FinanceBankAccount.php
+++ modules/Finance/Models/FinanceBankAccount.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Auditable as AuditableTrait;
-
+use App\Traits\ModelsTrait;
 /**
  * @class FinanceBank
  * @brief Datos de las cuentas bancarias
@@ -22,7 +22,7 @@ class FinanceBankAccount extends Model implements Auditable
 {
     use SoftDeletes;
     use AuditableTrait;
-
+    use ModelsTrait;
     /**
      * The attributes that should be mutated to dates.
      *
