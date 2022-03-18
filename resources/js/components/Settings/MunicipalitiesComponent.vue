aa0cbdbac7fa19960806ac875cca79d1d45f1c03
Argenis Osorio <argenisosorio580@hotmail.com>
2022-03-18 19:52:21 -0400

    Agregada validación y mensaje en Registro común de de Configuración

--- resources/js/components/Settings/MunicipalitiesComponent.vue
+++ resources/js/components/Settings/MunicipalitiesComponent.vue
@@ -1,144 +1,144 @@
 <template>
-	<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-2 mb-2 text-center">
-		<a class="btn-simplex btn-simplex-md btn-simplex-primary"
-		   href="javascript:void(0)" title="Registro de municipios"
-		   data-toggle="tooltip" @click="addRecord('add_municipality', 'municipalities', $event)">
-			<i class="icofont icofont-ui-map ico-3x"></i>
-			<span>Municipios</span>
-		</a>
-		<div class="modal fade text-left" tabindex="-1" role="dialog" id="add_municipality">
-			<div class="modal-dialog vue-crud" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">×</span>
-						</button>
-						<h6>
-							<i class="icofont icofont-ui-map inline-block"></i>
-							Municipios
-						</h6>
-					</div>
-					<div class="modal-body">
-						<form-errors :listErrors="errors"></form-errors>
-						<div class="row">
-							<div class="col-12 col-md-6">
-								<div class="form-group">
-									<label>Pais:</label>
-									<select2 :options="countries" @input="getEstates"
-											 v-model="record.country_id"></select2>
-									<input type="hidden" v-model="record.id">
-			                    </div>
-							</div>
-							<div class="col-12 col-md-6">
-								<div class="form-group">
-									<label>Estados:</label>
-									<select2 :options="estates" v-model="record.estate_id"></select2>
-			                    </div>
-							</div>
-							<div class="col-12 col-md-6">
-								<div class="form-group is-required">
-									<label>Código:</label>
-									<input type="text" placeholder="Código de Municipio" data-toggle="tooltip"
-										   title="Indique el código del Municipio (requerido)"
-										   class="form-control input-sm" v-model="record.code" v-is-digits>
-			                    </div>
-							</div>
-							<div class="col-12 col-md-6">
-								<div class="form-group is-required">
-									<label>Nombre:</label>
-									<input type="text" placeholder="Nombre de Municipio" data-toggle="tooltip"
-										   title="Indique el nombre del Municipio (requerido)"
-										   class="form-control input-sm" v-model="record.name" v-is-text>
-			                    </div>
-							</div>
-						</div>
-	                </div>
-	                <div class="modal-footer">
-	                	<div class="form-group">
-	                		<button type="button" class="btn btn-default btn-sm btn-round btn-modal-close" 
-									@click="clearFilters" data-dismiss="modal">
-								Cerrar
-							</button>
-							<button type="button" class="btn btn-warning btn-sm btn-round btn-modal btn-modal-clear" 
-									@click="reset()">
-								Cancelar
-							</button>
-							<button type="button" @click="createRecord('municipalities')" 
-									class="btn btn-primary btn-sm btn-round btn-modal-save">
-								Guardar
-							</button>
-	                	</div>
-	                </div>
-	                <div class="modal-body modal-table">
-	                	<v-client-table :columns="columns" :data="records" :options="table_options">
-	                		<div slot="id" slot-scope="props" class="text-center">
-	                			<button @click="initUpdate(props.row.id, $event)"
-		                				class="btn btn-warning btn-xs btn-icon btn-action"
-		                				title="Modificar registro" data-toggle="tooltip" type="button">
-		                			<i class="fa fa-edit"></i>
-		                		</button>
-		                		<button @click="deleteRecord(props.row.id, 'municipalities')"
-										class="btn btn-danger btn-xs btn-icon btn-action"
-										title="Eliminar registro" data-toggle="tooltip"
-										type="button">
-									<i class="fa fa-trash-o"></i>
-								</button>
-	                		</div>
-	                	</v-client-table>
-	                </div>
-		        </div>
-		    </div>
-		</div>
-	</div>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-2 mb-2 text-center">
+        <a class="btn-simplex btn-simplex-md btn-simplex-primary"
+           href="javascript:void(0)" title="Registro de municipios"
+           data-toggle="tooltip" @click="addRecord('add_municipality', 'municipalities', $event)">
+            <i class="icofont icofont-ui-map ico-3x"></i>
+            <span>Municipios</span>
+        </a>
+        <div class="modal fade text-left" tabindex="-1" role="dialog" id="add_municipality">
+            <div class="modal-dialog vue-crud" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                        <h6>
+                            <i class="icofont icofont-ui-map inline-block"></i>
+                            Municipios
+                        </h6>
+                    </div>
+                    <div class="modal-body">
+                        <form-errors :listErrors="errors"></form-errors>
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label>Pais:</label>
+                                    <select2 :options="countries" @input="getEstates"
+                                             v-model="record.country_id"></select2>
+                                    <input type="hidden" v-model="record.id">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label>Estados:</label>
+                                    <select2 :options="estates" v-model="record.estate_id"></select2>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group is-required">
+                                    <label>Código:</label>
+                                    <input type="text" placeholder="Código de Municipio" data-toggle="tooltip"
+                                           title="Indique el código del Municipio (requerido)"
+                                           class="form-control input-sm" v-model="record.code" v-is-digits>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group is-required">
+                                    <label>Nombre:</label>
+                                    <input type="text" placeholder="Nombre de Municipio" data-toggle="tooltip"
+                                           title="Indique el nombre del Municipio (requerido)"
+                                           class="form-control input-sm" v-model="record.name" v-is-text>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="form-group">
+                            <button type="button" class="btn btn-default btn-sm btn-round btn-modal-close" 
+                                    @click="clearFilters" data-dismiss="modal">
+                                Cerrar
+                            </button>
+                            <button type="button" class="btn btn-warning btn-sm btn-round btn-modal btn-modal-clear" 
+                                    @click="reset()">
+                                Cancelar
+                            </button>
+                            <button type="button" @click="createRecord('municipalities')" 
+                                    class="btn btn-primary btn-sm btn-round btn-modal-save">
+                                Guardar
+                            </button>
+                        </div>
+                    </div>
+                    <div class="modal-body modal-table">
+                        <v-client-table :columns="columns" :data="records" :options="table_options">
+                            <div slot="id" slot-scope="props" class="text-center">
+                                <button @click="initUpdate(props.row.id, $event)"
+                                        class="btn btn-warning btn-xs btn-icon btn-action"
+                                        title="Modificar registro" data-toggle="tooltip" type="button">
+                                    <i class="fa fa-edit"></i>
+                                </button>
+                                <button @click="deleteRecord(props.row.id, 'municipalities')"
+                                        class="btn btn-danger btn-xs btn-icon btn-action"
+                                        title="Eliminar registro" data-toggle="tooltip"
+                                        type="button">
+                                    <i class="fa fa-trash-o"></i>
+                                </button>
+                            </div>
+                        </v-client-table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				record: {
-					id: '',
-					country_id: '',
-					estate_id: '',
-					name: '',
-					code: ''
-				},
+    export default {
+        data() {
+            return {
+                record: {
+                    id: '',
+                    country_id: '',
+                    estate_id: '',
+                    name: '',
+                    code: ''
+                },
                 selectedEstateId: '',
-				errors: [],
-				records: [],
-				countries: [],
-				estates: {},
-				columns: ['estate.name', 'name', 'code', 'id'],
-			}
-		},
+                errors: [],
+                records: [],
+                countries: [],
+                estates: {},
+                columns: ['estate.name', 'name', 'code', 'id'],
+            }
+        },
         watch: {
             record: {
                 deep: true,
                 handler: function(newValue, oldValue) {
                     const vm = this;
                     if (vm.record.id) {
-                    	vm.record.estate_id = vm.selectedEstateId;
+                        vm.record.estate_id = vm.selectedEstateId;
                     }
                 }
             },
         },
-		methods: {
-			/**
-			 * Método que borra todos los datos del formulario
-			 *
-			 * @author  Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
-			 */
-			reset() {
+        methods: {
+            /**
+             * Método que borra todos los datos del formulario
+             *
+             * @author  Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+             */
+            reset() {
                 const vm = this;
-				vm.record = {
-					id: '',
-					country_id: '',
-					estate_id: '',
-					name: '',
-					code: ''
-				};
+                vm.record = {
+                    id: '',
+                    country_id: '',
+                    estate_id: '',
+                    name: '',
+                    code: ''
+                };
                 vm.selectedEstateId = '';
-			},
+            },
             /**
              * Método que carga el formulario con los datos a modificar
              *
@@ -159,28 +159,28 @@
                 vm.record.estate_id = vm.selectedEstateId;
                 event.preventDefault();
             }
-		},
-		created() {
-			this.table_options.headings = {
-				'estate.name': 'Estado',
-				'name': 'Municipio',
-				'code': 'Código',
-				'id': 'Acción'
-			};
-			this.table_options.sortable = ['estate.name', 'name', 'code'];
-			this.table_options.filterable = ['estate.name', 'name', 'code'];
-			this.table_options.columnsClasses = {
-				'estate.name': 'col-md-3',
-				'name': 'col-md-6',
-				'code': 'col-md-1',
-				'id': 'col-md-2'
-			};
-		},
-		mounted() {
-			let vm = this;
-			$("#add_municipality").on('show.bs.modal', function() {
-				vm.getCountries();
-			});
-		}
-	};
+        },
+        created() {
+            this.table_options.headings = {
+                'estate.name': 'Estado',
+                'name': 'Municipio',
+                'code': 'Código',
+                'id': 'Acción'
+            };
+            this.table_options.sortable = ['estate.name', 'name', 'code'];
+            this.table_options.filterable = ['estate.name', 'name', 'code'];
+            this.table_options.columnsClasses = {
+                'estate.name': 'col-md-3',
+                'name': 'col-md-6',
+                'code': 'col-md-1',
+                'id': 'col-md-2'
+            };
+        },
+        mounted() {
+            let vm = this;
+            $("#add_municipality").on('show.bs.modal', function() {
+                vm.getCountries();
+            });
+        }
+    };
 </script>
