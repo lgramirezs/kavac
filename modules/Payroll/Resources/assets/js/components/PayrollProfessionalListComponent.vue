dcfd8030ef5810f186bea916fc201e4a50a47a2b
José Briceño <josejorgebriceno9@gmail.com>
2022-04-11 11:48:48 -0400

    Issue #125: Actualizada la informacion presentada en el modal del boton ver informacion detallada ubicado en Talento humano > Expediente > Datos profesionales > Botón Ve.r

--- modules/Payroll/Resources/assets/js/components/PayrollProfessionalListComponent.vue
+++ modules/Payroll/Resources/assets/js/components/PayrollProfessionalListComponent.vue
@@ -65,7 +65,7 @@
                                         disabled="true" id="payroll_instruction_degree">
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <!-- <div class="col-md-4">
                                 <div class="form-group">
                                     <label>Profesiones</label>
                                     <v-multiselect :options="(record.professions) ? record.professions : []" track_by="name"
@@ -79,7 +79,7 @@
                                     <input type="text" data-toggle="tooltip" class="form-control input-sm"
                                         disabled="true" id="instruction_degree_name">
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
 
                         <hr>
@@ -96,7 +96,7 @@
                             </div>
     						<div class="col-3">
     							<div class="form-group is-required">
-    								<label>Anño de Graduación:</label>
+    								<label>Año de Graduación:</label>
     								<input type="text" class="form-control input-sm"
     									disabled="true" v-model="payroll_study.graduation_year"/>
     							</div>
