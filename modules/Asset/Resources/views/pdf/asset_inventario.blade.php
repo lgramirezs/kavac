38d29bf956ae5a0134ca1c742b28c9ff62d521a1
Pedro Buitrago <pbuitrago@cenditel.gob.ve>
2022-05-24 14:30:58 -0500

    Incidencia #207 Se agrego a la tabla de Reporte Historial de inventario la columna Especificaciones

--- modules/Asset/Resources/views/pdf/asset_inventario.blade.php
+++ modules/Asset/Resources/views/pdf/asset_inventario.blade.php
@@ -4,21 +4,22 @@
     <table cellspacing="0" cellpadding="1" border="0.5">
         <thead>
             <tr>
-                <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
-                <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Condición física</th>
+                <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
+                <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Condición física</th>
                 <th width="30%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Estatus de uso</th>
                 <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Serial</th>
                 <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Marca</th>
                 <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Modelo</th>
+                <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Especificación</th>
             </tr>
         </thead>
         <tbody>
             @foreach($asset->assetInventoryAssets as $assetInventoryAsset)
                 <tr>
-                    <td width="20%" style="font-size: 8rem;" align="center">
+                    <td width="10%" style="font-size: 8rem;" align="center">
                         {{ $assetInventoryAsset['asset']['institution']['name'] }}
                     </td>
-                    <td width="20%" style="font-size:9rem;" align="center">
+                    <td width="10%" style="font-size:9rem;" align="center">
                         {{ $assetInventoryAsset['asset']['assetCondition']['name'] }}
                     </td>
                     <td width="30%" style="font-size:9rem;" align="center">
@@ -38,6 +39,11 @@
                         {{ $assetInventoryAsset['asset']['model'] ?
                             $assetInventoryAsset['asset']['model'] :
                             '' }}
+                    </td>
+                     <td width="20%" style="font-size:9rem;" align="center">
+                        {{ str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) ?
+                                    str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) :
+                                    '' }}
                     </td>
                 </tr>
             @endforeach
@@ -61,29 +67,30 @@
         <table cellspacing="0" cellpadding="1" border="0.5">
             <thead>
                 <tr>
-                    <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
-                    <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Trabajador</th>
+                    <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
+                    <th width="15%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Trabajador</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Condición física</th>
-                    <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Estatus de uso</th>
+                    <th width="15%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Estatus de uso</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Serial</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Marca</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Modelo</th>
+                    <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Especificación</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($asset->assetInventoryAssets as $assetInventoryAsset)
                     @if($assetInventoryAsset->asset->assetAsignationAsset)
                         <tr>
-                            <td width="20%" style="font-size: 8rem;" align="center">
+                            <td width="10%" style="font-size: 8rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['institution']['name'] }}
                             </td>
-                            <td width="20%" style="font-size: 8rem;" align="center">
+                            <td width="15%" style="font-size: 8rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetAsignationAsset']['assetAsignation'] ? $assetInventoryAsset['asset']['assetAsignationAsset']['assetAsignation']['payrollStaff']['first_name'] . ' ' .  $assetInventoryAsset['asset']['assetAsignationAsset']['assetAsignation']['payrollStaff']['last_name'] : ''}}
                             </td>
                             <td width="10%" style="font-size:9rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetCondition']['name'] }}
                             </td>
-                            <td width="20%" style="font-size:9rem;" align="center">
+                            <td width="15%" style="font-size:9rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetStatus']['name'] }}
                             </td>
                             <td width="10%" style="font-size:9rem;" align="center">
@@ -99,6 +106,11 @@
                             <td width="10%" style="font-size:9rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['model'] ?
                                     $assetInventoryAsset['asset']['model'] :
+                                    '' }}
+                            </td>
+                            <td width="20%" style="font-size:9rem;" align="center">
+                                {{ str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) ?
+                                    str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) :
                                     '' }}
                             </td>
                         </tr>
@@ -127,13 +139,14 @@
         <table cellspacing="0" cellpadding="1" border="0.5">
             <thead>
                 <tr>
-                    <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
+                    <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Organización</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Condición física</th>
                     <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Estatus de uso</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Serial</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Marca</th>
                     <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Modelo</th>
                     <th width="20%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Motivo de desincorporación</th>
+                    <th width="10%" style="font-size:9rem; background-color: #BDBDBD;" align="center">Especificación</th>
                 </tr>
             </thead>
             <tbody>
@@ -143,13 +156,13 @@
                         $assetInventoryAsset['asset']['asset_status_id'] == 8 ||
                         $assetInventoryAsset['asset']['asset_status_id'] == 9)
                         <tr>
-                            <td width="20%" style="font-size: 8rem;" align="center">
+                            <td width="10%" style="font-size: 8rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['institution']['name'] }}
                             </td>
                             <td width="10%" style="font-size:9rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetCondition']['name'] }}
                             </td>
-                            <td width="20%" style="font-size:9rem;" align="center">
+                            <td width="10%" style="font-size:9rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetStatus']['name'] }}
                             </td>
                             <td width="10%" style="font-size:9rem;" align="center">
@@ -169,6 +182,11 @@
                             </td>
                             <td width="20%" style="font-size: 8rem;" align="center">
                                 {{ $assetInventoryAsset['asset']['assetDisincorporationAsset'] && $assetInventoryAsset['asset']['assetDisincorporationAsset']['assetDisincorporation'] ? $assetInventoryAsset['asset']['assetDisincorporationAsset']['assetDisincorporation']['assetDisincorporationMotive']['name'] : $assetInventoryAsset['asset']['assetStatus']['name'] }}
+                            </td>
+                            <td width="10%" style="font-size:9rem;" align="center">
+                                {{ str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) ?
+                                    str_replace("<p>", "", str_replace("</p>","",$assetInventoryAsset['asset']['specifications'])) :
+                                    '' }}
                             </td>
                         </tr>
                     @endif
