86b9030487f059bf7de42c3491c7c8bcb5686917
Argenis Osorio <argenisosorio10@gmail.com>
2022-06-21 12:40:16 -0400

    Eliminada restricción de rol en opción Gestión y Firmado del menú de Firma electrónica

--- modules/DigitalSignature/Resources/views/layouts/menu-option.blade.php
+++ modules/DigitalSignature/Resources/views/layouts/menu-option.blade.php
@@ -4,8 +4,6 @@
         <i class="icofont icofont-ui-password"></i><span> Firma Electrónica </span>
     </a>
     <ul class="submenu" style="{!! display_submenu('digitalsignature') !!}">
-        @role(['admin','digitalsignature'])
-            <li> <a href="{{ route('digitalsignature') }}"> Gestión y firmado </a> </li>
-        @endrole
+        <li><a href="{{ route('digitalsignature') }}">Gestión y firmado</a></li>
     </ul>
 </li>
