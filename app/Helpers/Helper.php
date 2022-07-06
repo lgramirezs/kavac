46859a6a4193a59af35c0c1f59aba0b0b810f3a2
Ing. Roldan Vargas <roldandvg@gmail.com>
2022-07-05 22:34:35 -0400

    se agregan funciones para convertir un valor numérico a letras

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -912,3 +912,350 @@ if (! function_exists('isModuleEnabled')) {
         return Module::collections()->has($moduleName);
     }
 }
+
+
+// =================== Números a letras ===================
+if (! function_exists('unidad')) {
+    function unidad($numuero) {
+        switch (floor($numuero)) {
+            case 9: {
+                $numu = "NUEVE";
+                break;
+            }
+            case 8: {
+                $numu = "OCHO";
+                break;
+            }
+            case 7: {
+                $numu = "SIETE";
+                break;
+            }
+            case 6: {
+                $numu = "SEIS";
+                break;
+            }
+            case 5: {
+                $numu = "CINCO";
+                break;
+            }
+            case 4: {
+                $numu = "CUATRO";
+                break;
+            }
+            case 3: {
+                $numu = "TRES";
+                break;
+            }
+            case 2: {
+                $numu = "DOS";
+                break;
+            }
+            case 1: {
+                $numu = "UN";
+                break;
+            }
+            case 0: {
+                $numu = "";
+                break;
+            }
+        }
+        return $numu;
+    }
+}
+
+if (! function_exists('decena')) {
+    function decena($numdero) {
+        $numdero=floor($numdero);
+        if ($numdero >= 90 && $numdero <= 99) {
+            $numd = "NOVENTA ";
+            if ($numdero > 90) {
+                $numd = $numd."Y ".(unidad($numdero - 90));
+            }
+        } elseif ($numdero >= 80 && $numdero <= 89) {
+            $numd = "OCHENTA ";
+            if ($numdero > 80) {
+                $numd = $numd."Y ".(unidad($numdero - 80));
+            }
+        } elseif ($numdero >= 70 && $numdero <= 79) {
+            $numd = "SETENTA ";
+            if ($numdero > 70) {
+                $numd = $numd."Y ".(unidad($numdero - 70));
+            }
+        } elseif ($numdero >= 60 && $numdero <= 69) {
+            $numd = "SESENTA ";
+            if ($numdero > 60) {
+                $numd = $numd."Y ".(unidad($numdero - 60));
+            }
+        } elseif ($numdero >= 50 && $numdero <= 59) {
+            $numd = "CINCUENTA ";
+            if ($numdero > 50) {
+                $numd = $numd."Y ".(unidad($numdero - 50));
+            }
+        } elseif ($numdero >= 40 && $numdero <= 49) {
+            $numd = "CUARENTA ";
+            if ($numdero > 40) {
+                $numd = $numd."Y ".(unidad($numdero - 40));
+            }
+        } elseif ($numdero >= 30 && $numdero <= 39) {
+            $numd = "TREINTA ";
+            if ($numdero > 30) {
+                $numd = $numd."Y ".(unidad($numdero - 30));
+            }
+        } elseif ($numdero >= 20 && $numdero <= 29) {
+            if ($numdero == 20) {
+                $numd = "VEINTE ";
+            } else {
+                $numd = "VEINTI".(unidad($numdero - 20));
+            }
+        } elseif ($numdero >= 10 && $numdero <= 19) {
+            switch ($numdero) {
+                case 10: {
+                    $numd = "DIEZ ";
+                    break;
+                }
+                case 11: {
+                    $numd = "ONCE ";
+                    break;
+                }
+                case 12: {
+                    $numd = "DOCE ";
+                    break;
+                }
+                case 13: {
+                    $numd = "TRECE ";
+                    break;
+                }
+                case 14: {
+                    $numd = "CATORCE ";
+                    break;
+                }
+                case 15: {
+                    $numd = "QUINCE ";
+                    break;
+                }
+                case 16: {
+                    $numd = "DIECISEIS ";
+                    break;
+                }
+                case 17: {
+                    $numd = "DIECISIETE ";
+                    break;
+                }
+                case 18: {
+                    $numd = "DIECIOCHO ";
+                    break;
+                }
+                case 19: {
+                    $numd = "DIECINUEVE ";
+                    break;
+                }
+            }
+        } else {
+            $numd = unidad($numdero);
+        }
+
+        return $numd;
+    }
+}
+
+if (! function_exists('centena')) {
+    function centena($numc) {
+        $numc=floor($numc);
+        if ($numc >= 100) {
+            if ($numc >= 900 && $numc <= 999) {
+                $numce = "NOVECIENTOS ";
+                if ($numc > 900) {
+                    $numce = $numce.(decena($numc - 900));
+                }
+            } elseif ($numc >= 800 && $numc <= 899) {
+                $numce = "OCHOCIENTOS ";
+                if ($numc > 800) {
+                    $numce = $numce.(decena($numc - 800));
+                }
+            } elseif ($numc >= 700 && $numc <= 799) {
+                $numce = "SETECIENTOS ";
+                if ($numc > 700) {
+                    $numce = $numce.(decena($numc - 700));
+                }
+            } elseif ($numc >= 600 && $numc <= 699) {
+                $numce = "SEISCIENTOS ";
+                if ($numc > 600) {
+                    $numce = $numce.(decena($numc - 600));
+                }
+            } elseif ($numc >= 500 && $numc <= 599) {
+                $numce = "QUINIENTOS ";
+                if ($numc > 500) {
+                    $numce = $numce.(decena($numc - 500));
+                }
+            } elseif ($numc >= 400 && $numc <= 499) {
+                $numce = "CUATROCIENTOS ";
+                if ($numc > 400) {
+                    $numce = $numce.(decena($numc - 400));
+                }
+            } elseif ($numc >= 300 && $numc <= 399) {
+                $numce = "TRESCIENTOS ";
+                if ($numc > 300) {
+                    $numce = $numce.(decena($numc - 300));
+                }
+            } elseif ($numc >= 200 && $numc <= 299) {
+                $numce = "DOSCIENTOS ";
+                if ($numc > 200) {
+                    $numce = $numce.(decena($numc - 200));
+                }
+            } elseif ($numc >= 100 && $numc <= 199) {
+                if ($numc == 100) {
+                    $numce = "CIEN ";
+                } else {
+                    $numce = "CIENTO ".(decena($numc - 100));
+                }
+            }
+        } else {
+            $numce = decena($numc);
+        }
+
+        return $numce;
+    }
+}
+
+if (! function_exists('miles')) {
+    function miles($nummero) {
+        $nummero=floor($nummero);
+        if ($nummero >= 1000 && $nummero < 2000) {
+            $numm = "MIL ".(centena($nummero%1000));
+        }
+        if ($nummero >= 2000 && $nummero <10000) {
+            $numm = unidad(Floor($nummero/1000))." MIL ".(centena($nummero%1000));
+        }
+        if ($nummero < 1000) {
+            $numm = centena($nummero);
+        }
+
+        return $numm;
+    }
+}
+
+if (! function_exists('decmiles')) {
+    function decmiles($numdmero) {
+        if ($numdmero == 10000) {
+            $numde = "DIEZ MIL";
+        }
+        if ($numdmero > 10000 && $numdmero <20000) {
+            $numde = decena(Floor($numdmero/1000))."MIL ".(centena($numdmero%1000));
+        }
+        if ($numdmero >= 20000 && $numdmero <100000) {
+            $numde = decena(Floor($numdmero/1000))." MIL ".(miles($numdmero%1000));
+        }
+        if ($numdmero < 10000) {
+            $numde = miles($numdmero);
+        }
+
+        return $numde;
+    }
+}
+
+if (! function_exists('cienmiles')) {
+    function cienmiles($numcmero) {
+        if ($numcmero == 100000) {
+            $num_letracm = "CIEN MIL";
+        }
+        if ($numcmero >= 100000 && $numcmero <1000000) {
+            $num_letracm = centena(Floor($numcmero/1000))." MIL ".(centena((int)$numcmero%1000));
+        }
+        if ($numcmero < 100000) {
+            $num_letracm = decmiles($numcmero);
+        }
+
+        return $num_letracm;
+    }
+}
+
+if (! function_exists('millon')) {
+    function millon($nummiero) {
+        if (((int)($nummiero)%1000000)==0) {
+            $deletras='DE ';
+        } else {
+            $deletras='';
+        }
+        if ($nummiero >= 1000000 && $nummiero <2000000) {
+            $num_letramm = "UN MILLON ".$deletras.(cienmiles($nummiero%1000000));
+        }
+        if ($nummiero >= 2000000 && $nummiero <10000000) {
+            $num_letramm = unidad(Floor($nummiero/1000000))." MILLONES ".$deletras.(cienmiles($nummiero%1000000));
+        }
+        if ($nummiero < 1000000) {
+            $num_letramm = cienmiles($nummiero);
+        }
+        
+        return $num_letramm;
+    }
+}
+
+if (! function_exists(('decmillon'))) {
+    function decmillon($numerodm) {
+        if (abs(Floor($numerodm/1000000))==0) {
+            $deletras='DE ';
+        } else {
+            $deletras='';
+        }
+        if ($numerodm == 10000000) {
+            $num_letradmm = "DIEZ MILLONES".$deletras;
+        }
+        if ($numerodm > 10000000 && $numerodm <20000000) {
+            $num_letradmm = decena(Floor($numerodm/1000000))."MILLONES ".$deletras.(cienmiles($numerodm%1000000));
+        }
+        if ($numerodm >= 20000000 && $numerodm <100000000) {
+            $num_letradmm = decena(Floor($numerodm/1000000))." MILLONES ".$deletras.(millon($numerodm%1000000));
+        }
+        if ($numerodm < 10000000) {
+            $num_letradmm = millon($numerodm);
+        }
+
+        return $num_letradmm;
+    }
+}
+
+if (! function_exists('cienmillon')) {
+    function cienmillon($numcmeros) {
+        if (abs(Floor($numcmeros/1000000))==0) {
+            $deletras='DE ';
+        } else {
+            $deletras='';
+        }
+        if ($numcmeros == 100000000) {
+            $num_letracms = "CIEN MILLONES".$deletras;
+        }
+        if ($numcmeros >= 100000000 && $numcmeros <1000000000) {
+            $num_letracms = centena(Floor($numcmeros/1000000))." MILLONES ".$deletras.(millon($numcmeros%1000000));
+        }
+        if ($numcmeros < 100000000) {
+            $num_letracms = decmillon($numcmeros);
+        }
+
+        return $num_letracms;
+    }
+}
+
+if (! function_exists('milmillon')) {
+    function milmillon($nummierod) {
+        if ($nummierod >= 1000000000 && $nummierod <2000000000) {
+            $num_letrammd = "MIL ".(cienmillon($nummierod%1000000000));
+        }
+        if ($nummierod >= 2000000000 && $nummierod <10000000000) {
+            $num_letrammd = unidad(Floor($nummierod/1000000000))." MIL ".(cienmillon($nummierod%1000000000));
+        }
+        if ($nummierod < 1000000000) {
+            $num_letrammd = cienmillon($nummierod);
+        }
+
+        return $num_letrammd;
+    }
+}
+
+if (! function_exists('convertirNumeros')) {
+    function convertirNumeros($numero) {
+        $numf = milmillon($numero);
+        $explodeNumber = explode(".", $numero);
+        $decimals = (isset($explodeNumber[1]))?"$explodeNumber[1]/100":"00/100";
+        return $numf." BOLIVARES CON $decimals";
+    }
+}
