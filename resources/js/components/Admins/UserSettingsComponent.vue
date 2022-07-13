d394568f6e904de630298ffe6458261537dcc274
Ing. Roldan Vargas <roldandvg@gmail.com>
2022-07-13 07:42:49 -0400

    reemplazo de elemento switch

--- resources/js/components/Admins/UserSettingsComponent.vue
+++ resources/js/components/Admins/UserSettingsComponent.vue
@@ -12,32 +12,25 @@
                 <div class="modal-body">
                     <div class="row">
                         <div class="col-12 col-md-4">
-                            <label for="">
-                                <div class="bootstrap-switch-mini">
+                            <div class="custom-control custom-switch" data-toggle="tooltip" 
+                                 title="Bloquear / Desbloquear usuario" >
+                                <input type="checkbox" class="custom-control-input" id="settingUserBlock" 
+                                       v-model="record.blocked_at" :value="true">
+                                <label class="custom-control-label" for="settingUserBlock">
                                     Bloquear / Desbloquear usuario
-                                    <input type="checkbox" class="form-control bootstrap-switch"
-                                           name="blocked_at" id="blocked_at" data-toggle="tooltip"
-                                           data-on-label="SI" data-off-label="NO"
-                                           title="Bloquear / Desbloquear usuario"
-                                           v-model="record.blocked_at" value="true"
-                                           data-record="blocked_at">
-                                </div>
-                            </label>
+                                </label>
+                            </div>
                         </div>
                         <div class="col-12 col-md-4">
-                            <label for="">
-                                <div class="bootstrap-switch-mini">
+                            <div class="custom-control custom-switch" data-toggle="tooltip" 
+                                 title="Activar / Desactivar usuario" >
+                                <input type="checkbox" class="custom-control-input" id="settingUserActive" 
+                                       v-model="record.active" :value="true">
+                                <label class="custom-control-label" for="settingUserActive">
                                     Activar / Desactivar usuario
-                                    <input type="checkbox" class="form-control bootstrap-switch"
-                                           name="active" id="active" data-toggle="tooltip"
-                                           data-on-label="SI" data-off-label="NO"
-                                           title="Bloquear / Desactivar usuario"
-                                           v-model="record.active" value="true"
-                                           data-record="active">
-                                </div>
-                            </label>
+                                </label>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-4"></div>
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -70,8 +63,6 @@
                     vm.record.userId = id;
                     vm.record.blocked_at = (response.data.user.blocked_at !== null) ? true : false;
                     vm.record.active = (response.data.user.active===true)?true:false;
-                    $("#blocked_at").bootstrapSwitch('state', vm.record.blocked_at);
-                    $("#active").bootstrapSwitch('state', vm.record.active);
                 }).catch(error => {
                     console.error(error);
                 });
@@ -96,8 +87,7 @@
         },
         mounted() {
             const vm = this;
-            vm.switchHandler('blocked_at');
-            vm.switchHandler('active');
+            
             $("#modalUserSettings").on("show.bs.modal", function () {
                 vm.getUserInfo(window.userId || '');
             });
@@ -107,8 +97,6 @@
                     blocked_at: false,
                     active: false
                 };
-                $("#blocked_at").bootstrapSwitch('state', false);
-                $("#active").bootstrapSwitch('state', false);
             });
         }
     };
