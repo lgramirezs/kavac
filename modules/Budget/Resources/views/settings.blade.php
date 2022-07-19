dae20f11f738194c2e02f3360acda69de2f559ae
Argenis Osorio <argenisosorio580@hotmail.com>
2022-07-19 19:08:53 -0400

    Agregada sección para los registros comúnes en Presupuesto > Configuración

--- modules/Budget/Resources/views/settings.blade.php
+++ modules/Budget/Resources/views/settings.blade.php
@@ -17,6 +17,7 @@
 @stop
 
 @section('content')
+    <!-- Formatos de códigos -->
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -141,6 +142,29 @@
             </div>
         </div>
     </div>
+    <!-- Final Formatos de códigos -->
+
+    <!-- Registros comunes -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h6 class="card-title">Registros comunes</h6>
+                    <div class="card-btns">
+                        @include('buttons.previous', ['route' => url()->previous()])
+                        @include('buttons.minimize')
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Final Registros comunes -->
+
+    <!-- Proyectos -->
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -164,6 +188,9 @@
             </div>
         </div>
     </div>
+    <!-- Final Proyectos -->
+
+    <!-- Acciones centralizadas -->
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -187,6 +214,9 @@
             </div>
         </div>
     </div>
+    <!-- Final Acciones centralizadas -->
+
+    <!-- Acciones específicas -->
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -210,4 +240,5 @@
             </div>
         </div>
     </div>
+    <!-- Final Acciones específicas -->
 @stop
