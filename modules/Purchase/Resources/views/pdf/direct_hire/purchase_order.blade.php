eb91f6034837aa0ea07635e8eae40a9cc0eb4a31
Juan Rosas <juan.rosasr01@gmail.com>
2022-07-20 12:25:08 -0400

    Compras: Ajustes de rutas para pdf

---
+++ modules/Purchase/Resources/views/pdf/direct_hire/purchase_order.blade.php
@@ -0,0 +1,16 @@
+<table cellspacing="1" cellpadding="0" border="0">
+  <thead>
+  </thead>
+  <tbody>
+    <tr>
+      <td width="60%">
+        Descripción de contratación: {{ $record->description }}
+      </td>
+      <td width="40%">
+        Nro de contratación {{ $record->code }}
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+{{$record}}
